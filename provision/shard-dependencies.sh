47288d701e23afb943a7a9e36d61847133f1dc45
Tobias Peters <tobias.peters@kreativeffekt.at>
2015-12-14 09:03:32 +0100

    - set all install selections for shard dependencies and re add dependencies

--- provision/shard-dependencies.sh
+++ provision/shard-dependencies.sh
@@ -4,6 +4,7 @@ apt-get -qq update
 
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/setup-password password root'
@@ -13,15 +14,13 @@ debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multisel
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
 
 
-apt-get -y install mysql-server phpmyadmin
+apt-get -y install \
+apache2 \
+php5 \
+php5-mcrypt \
+mysql-server \
+phpmyadmin \
+cmake \
+mercurial
 
-#apt-get -y install \
-#apache2 \
-#php5 \
-#php5-mcrypt \
-#mysql-server \
-#phpmyadmin \
-#cmake \
-#mercurial
-#
-#php5enmod mcrypt
+php5enmod mcrypt
