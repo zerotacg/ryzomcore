0720324bdcf7ffc54a8424dbcd86888b463182a0
Tobias Peters <tobias.peters@kreativeffekt.at>
2015-12-14 22:34:00 +0100

    - fixed service executables  - added basic shard data and links to bring up backup service

--- provision/shard-data.sh
+++ provision/shard-data.sh
@@ -2,4 +2,7 @@
 
 source /vagrant/provision/.profile
 
-hg clone https://@bitbucket.org/ryzom/ryzomcore-data ${SHARD_PATH}/ryzomcore-data
+SHARD_DATA_PATH=${SHARD_PATH}/ryzomcore-data
+hg clone https://@bitbucket.org/ryzom/ryzomcore-data ${SHARD_DATA_PATH}
+
+ln -s ${SHARD_DATA_PATH}/leveldesign/game_elem/sheet_id.bin ${SHARD_PATH}/backup_service/sheet_id.bin
