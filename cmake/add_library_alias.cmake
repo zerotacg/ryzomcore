22a4e9200ddd5fd4e007e0d487c9f53dcd614b2c
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-07-06 07:44:44 +0000

    remove unused code and add required components to snowballs project

--- cmake/add_library_alias.cmake
+++ cmake/add_library_alias.cmake
@@ -1,5 +1,4 @@
 function(add_library_project_alias target alias)
-    string(TOLOWER ${PROJECT_NAME} _LOWER_PROJECT_NAME)
     set_target_properties(${target} PROPERTIES EXPORT_NAME "${alias}")
 
     add_library(${PROJECT_NAME}::${alias} ALIAS ${target})
