7db596050b403c1858f280e55a89278261316db4
Tobias Peters <tobias.peters@kreativeffekt.at>
2025-08-18 06:15:35 +0000

    rename cmake function

--- cmake/add_project_library_alias.cmake
+++ cmake/add_project_library_alias.cmake
@@ -1,4 +1,4 @@
-function(add_library_project_alias target alias)
+function(add_project_library_alias target alias)
     set_target_properties(${target} PROPERTIES EXPORT_NAME "${alias}")
 
     add_library(${PROJECT_NAME}::${alias} ALIAS ${target})
