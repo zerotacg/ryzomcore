d3ffa3715b20a675958b12a2c856a4cf2adbec74
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-01-25 22:00:47 +0100

    move lua imported target into luabind module

--- CMakeModules/FindLuabind.cmake
+++ CMakeModules/FindLuabind.cmake
@@ -99,7 +99,7 @@ MACRO(FIND_CORRECT_LUA_VERSION)
       FIND_PACKAGE(Lua REQUIRED 5.0)
     ENDIF()
   ENDIF()
-  if(Lua_FOUND AND NOT TARGET Lua::Lua)
+  if((Lua_FOUND OR LUA_FOUND) AND NOT TARGET Lua::Lua)
       add_library(Lua::Lua INTERFACE IMPORTED)
       set_target_properties(
               Lua::Lua
