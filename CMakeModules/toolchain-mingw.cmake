bf544eb0144aa9ca6cad3158fbfff8c23fdb8e05
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-02-02 19:08:54 +0100

    adjust dependencies and add workaround for mingw zlib issue

--- CMakeModules/toolchain-mingw.cmake
+++ CMakeModules/toolchain-mingw.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # where is the target environment
-set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32;${CMAKE_FIND_ROOT_PATH}) # workaround for check in vcpkg's zlib port)
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
