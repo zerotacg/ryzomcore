316afcb15519d7912a20ad8ec61feef291cc1465
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-02 17:13:58 +0100

    add hunter setup and use it in favor of including hunter inside the project

---
+++ cmake/SetupHunter.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
+
+set(HUNTER_PACKAGES CURL freetype giflib Jpeg PNG libogg vorbis ffmpeg OpenAL libxml2 Lua luabind OpenSSL ZLIB)
+
+set(HUNTER_URL "https://github.com/cpp-pm/hunter/archive/v0.26.1.tar.gz")
+set(HUNTER_SHA1 "e41ac7a18c49b35ebac99ff2b5244317b2638a65")
+set(HUNTER_FILEPATH_CONFIG "${CMAKE_CURRENT_LIST_DIR}/CMakeModules/HunterConfig.cmake")
+
+include(FetchContent)
+FetchContent_Declare(SetupHunter GIT_REPOSITORY https://github.com/cpp-pm/gate)
+FetchContent_MakeAvailable(SetupHunter)
