f4a33fbff3777c4e4a830e39f8d8d23040e6f1ff
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-03 18:36:10 +0100

    use cmake alias target

--- CMakeModules/FindHelpers.cmake
+++ CMakeModules/FindHelpers.cmake
@@ -924,7 +924,7 @@ MACRO(FIND_QT5)
       FIND_PACKAGE(Freetype)
 
       IF(FREETYPE_FOUND)
-        LIST(APPEND QT_LIBRARIES ${FREETYPE_LIBRARIES})
+        LIST(APPEND QT_LIBRARIES Freetype::Freetype)
       ELSE()
         IF(UNIX)
           SET(FREETYPE_LIB "${QT_LIBRARY_DIR}/libqtfreetype.a")
